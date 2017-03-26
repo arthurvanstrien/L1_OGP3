35b1fe53d8f73bf7e84315f6e00c84a12f820d01
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-03-26 21:21:57 +0200

    Started to work on the module Comparable Practice.

---
+++ OGP3_ComparablePractice/src/ComparablePractice.java
@@ -0,0 +1,26 @@
+import java.lang.Comparable;
+
+/**
+ * Created by Arthur on 26-3-2017.
+ */
+public abstract class ComparablePractice implements Comparable
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public ComparablePractice()
+    {
+
+    }
+
+    public void CompareTo(Object T)
+    {
+
+    }
+
+
+
+
+}
